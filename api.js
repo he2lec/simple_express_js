dbc662999b2694f349ce6230ede40eba3b23dba0
Adrien Hellec <adrien.hellec@epita.fr>
2020-01-13 16:19:26 +0100

    Handle master/slave databases

--- api.js
+++ api.js
@@ -1,11 +1,12 @@
 'user strict';
 
-var pool = require('./db.js');
+var api = require('./db.js');
 
 const getUsers = (request, response) => {
-  pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+  api.queryNoValues('SELECT * FROM users ORDER BY id ASC', (error, results) => {
     if (error) {
-      throw error
+      response.status(501).json({"error": true})
+      return
     }
     response.status(200).json(results.rows)
   })
@@ -14,9 +15,10 @@ const getUsers = (request, response) => {
 const getUserById = (request, response) => {
   const id = parseInt(request.params.id)
 
-  pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+  api.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
     if (error) {
-      throw error
+      response.status(501).json({"error": true})
+      return
     }
     response.status(200).json(results.rows)
   })
@@ -25,9 +27,10 @@ const getUserById = (request, response) => {
 const createUser = (request, response) => {
   const { name, email } = request.body
 
-  pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
+  api.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
     if (error) {
-      throw error
+      response.status(501).json({"error": true})
+      return
     }
     response.status(201).send(`User added with ID: ${result.insertId}`)
   })
@@ -37,12 +40,13 @@ const updateUser = (request, response) => {
   const id = parseInt(request.params.id)
   const { name, email } = request.body
 
-  pool.query(
+  api.query(
     'UPDATE users SET name = $1, email = $2 WHERE id = $3',
     [name, email, id],
     (error, results) => {
       if (error) {
-        throw error
+        response.status(501).json({"error": true})
+        return
       }
       response.status(200).send(`User modified with ID: ${id}`)
     }
@@ -52,9 +56,10 @@ const updateUser = (request, response) => {
 const deleteUser = (request, response) => {
   const id = parseInt(request.params.id)
 
-  pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+  api.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
     if (error) {
-      throw error
+      response.status(501).json({"error": true})
+      return
     }
     response.status(200).send(`User deleted with ID: ${id}`)
   })
--- db.js
+++ db.js
@@ -3,14 +3,61 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+const pool_master = new Pool({
+  user: process.env.DB__MASTER_USER,
+  host: process.env.DB__MASTER_HOST,
+  database: process.env.DB_MASTER_DATABASE,
+  password: process.env.DB_MASTER_PASSWORD,
+  port: process.env.DB_MASTER_PORT,
 })
 
-pool.connect();
+const pool_slave = new Pool({
+  user: process.env.DB_SLAVE_USER,
+  host: process.env.DB_SLAVE_HOST,
+  database: process.env.DB_SLAVE_DATABASE,
+  password: process.env.DB_SLAVE_PASSWORD,
+  port: process.env.DB_SLAVE_PORT,
+})
 
-module.exports = pool;
+const query = (queryText, values,callback) => {
+  pool_master.connect().catch((err) => console.log("Failed to connect to master db : " + err.message));
+  pool_slave.connect().catch((err) => console.log("Failed to connect to slave db : " + err.message));
+
+  pool_master.query(queryText, values, (error_master, results_master) => {
+    if (error_master) {
+      pool_slave.query(queryText, values, (error_slave, results_slave) => {
+        if (error_slave) {
+          callback(true, null)
+          return
+        }
+        callback(false, results_slave)
+      })
+      return
+    }
+    callback(false, results_master)
+  })
+}
+
+const queryNoValues = (queryText, callback) => {
+  pool_master.connect().catch((err) => console.log("Failed to connect to master db : " + err.message));
+  pool_slave.connect().catch((err) => console.log("Failed to connect to slave db : " + err.message));
+
+  pool_master.query(queryText, (error_master, results_master) => {
+    if (error_master) {
+      pool_slave.query(queryText, (error_slave, results_slave) => {
+        if (error_slave) {
+          callback(true, null)
+          return
+        }
+        callback(false, results_slave)
+      })
+      return
+    }
+    callback(false, results_master)
+  })
+}
+
+module.exports = {
+  query,
+  queryNoValues
+ } ;
